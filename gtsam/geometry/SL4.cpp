fe1317aea7f3a7620419550734abe8c8319e45b2
Hyungtae Lim <shapelim@mit.edu>
2025-05-03 15:55:00 -0400

    Upload SL4 to geometry folder

---
+++ gtsam/geometry/SL4.cpp
@@ -0,0 +1,147 @@
+/**
+ * @file  SL4.cpp
+ * @brief Projective Special Linear Group (PSL(4, R)) Pose
+ * @author: Hyungtae Lim
+ */
+
+#include <sl4/SL4.h>
+
+
+using namespace std;
+
+namespace gtsam {
+
+
+SL4::SL4(const Matrix44& pose) {
+  double det = pose.determinant();
+  if (det <= 0.0) {
+    throw std::runtime_error("Matrix determinant must be positive for SL(4) normalization. Got det = " + std::to_string(det));
+  }
+
+  T_ = pose / std::pow(det, 1.0 / 4.0);
+}
+
+SL4::SL4(const SL4& pose)
+    : T_(pose.T_) {}
+
+/* ************************************************************************* */
+void SL4::print(const std::string& s) const {
+  cout << s << T_ << "\n";
+}
+
+/* ************************************************************************* */
+bool SL4::equals(const SL4& psl4, double tol) const {
+  return T_.isApprox(psl4.T_, tol);
+}
+
+/* ************************************************************************* */
+// NOTE(hlim): In PGO, this function is not used
+SL4 SL4::inverse(SL4Jacobian H1) const {
+  if (!H1) {
+    return SL4(T_.inverse());
+  }
+
+  // TODO(hlim): Might not affect the PGO quality at all,  
+  // but should be implemented for the complete implementation
+  SL4 result(T_.inverse());
+  throw std::runtime_error("H matrix is not implemented.");
+  return result;
+}
+
+/* ************************************************************************* */
+// NOTE(hlim): In PGO, this function is not used
+SL4 SL4::compose(const SL4& p2, SL4Jacobian H1,
+                           SL4Jacobian H2) const {
+  if (!H1 && !H2) return SL4(T_ * p2.T_);
+
+  // TODO(hlim): Might not affect the PGO quality at all,  
+  // but should be implemented for the complete implementation
+  SL4 result(T_ * p2.T_);
+  throw std::runtime_error("H matrix is not implemented.");
+  return result;
+}
+
+/* ************************************************************************* */
+SL4 SL4::between(const SL4& p2, SL4Jacobian H1,
+                           SL4Jacobian H2) const {
+  if (!H1 && !H2) return SL4(T_.inverse() * p2.T_);
+
+  SL4 result(T_.inverse() * p2.T_);
+  if (H1) {
+    *H1 = -result.inverse().AdjointMap();
+  }
+  if (H2) *H2 = I_15x15;
+  return result;
+}
+
+SL4 SL4::retract(const Vector& v, SL4Jacobian Horigin,
+                           SL4Jacobian Hv) const {
+  assert(v.size() == 15);
+
+  SL4 retracted_pose = SL4(T_ * (I_4x4 + vectorToAlgebra(v)));
+
+  if (Horigin) {
+    // TODO(hlim) Should be implemented
+      throw std::runtime_error("H matrix is not implemented.");
+    *Horigin = I_15x15;
+  }
+
+  if (Hv) {
+    // TODO(hlim) This matrix should be double checked
+    throw std::runtime_error("H matrix is not implemented.");
+    *Hv = I_15x15;
+  }
+
+  return retracted_pose;
+}
+
+Vector SL4::localCoordinates(const SL4& p2,
+                              SL4Jacobian Horigin,
+                              SL4Jacobian Hp2) const {
+  OptionalJacobian<3, 3>::Jacobian H3x3_1, H3x3_2;
+  Vector result = SL4::Logmap(T_.inverse() * p2.T_);
+
+  if (Horigin) {
+    // TODO(hlim) This matrix should be double checked
+    throw std::runtime_error("H matrix is not implemented.");
+    *Horigin = -I_15x15;
+  }
+
+  if (Hp2) {
+    // TODO(hlim) This matrix should be double checked
+    throw std::runtime_error("H matrix is not implemented.");
+    *Hp2 = I_15x15;
+  }
+
+  return result;
+}
+
+/* ************************************************************************* */
+SL4 SL4::Expmap(const Vector& xi) {
+  assert(xi.size() == 15);
+  const auto & mat = vectorToAlgebra(xi);
+  
+  // NOTE(hlim):
+  // The cost of the computation is approximately 20n^3 for matrices of size n.
+  // The number 20 depends weakly on the norm of the matrix.
+  // See https://eigen.tuxfamily.org/dox/unsupported/group__MatrixFunctions__Module.html
+  
+  // TODO(hlim): Approximate exp function? But it introduces non-negligible numerical error.
+  // return SL4(Matrix44::Identity() + mat + 0.5 * mat * mat);
+
+  return SL4(mat.exp());
+}
+
+/* ************************************************************************* */
+Vector SL4::Logmap(const Matrix44& T) {
+  const Matrix44 &mat = T.log();
+  
+  return algebraToVector(mat);
+}
+
+Vector SL4::Logmap(const SL4& p) {
+  return Logmap(p.T_);
+}
+/* ************************************************************************* */
+
+}  // namespace gtsam
---
+++ gtsam/geometry/SL4.h
@@ -0,0 +1,250 @@
+/**j
+ * @file  SL4.h
+ * @brief Projective Special Linear Group (SL(4, R)) factor
+ * @author: Hyungtae Lim
+ */
+
+#pragma once
+
+#include <gtsam/geometry/Pose2.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam_unstable/dllexport.h>
+#include <gtsam/base/OptionalJacobian.h>
+#include <gtsam/base/Vector.h>
+#include <gtsam/base/Matrix.h>
+
+// To use exp(), log()
+#include <unsupported/Eigen/MatrixFunctions>
+
+#include <string>
+
+using SL4Jacobian = gtsam::OptionalJacobian<15, 15>;
+
+inline Eigen::Matrix<double, 15, 15> I_15x15 = Eigen::Matrix<double, 15, 15>::Identity();
+
+using Matrix15x15 = Eigen::Matrix<double, 15, 15>;
+using Matrix16x16 = Eigen::Matrix<double, 16, 16>;
+
+inline Eigen::Matrix<double, 16, 15> setVecToAlgMatrix() {
+  Eigen::Matrix<double, 16, 15> alg = Eigen::Matrix<double, 16, 15>::Zero();
+
+  // 12 Off-diagonal E_ij generators
+  int k = 0;
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      if (i != j) {
+        alg(i * 4 + j, k++) = 1.0;
+      }
+    }
+  }
+
+  // For Diagonal generators B1 = diag(1, -1, 0, 0)
+  alg(0, 12) = 1.0;
+  alg(5, 12) = -1.0;
+
+  // For B2 = diag(0, 1, -1, 0)
+  alg(5, 13)  = 1.0;
+  alg(10, 13) = -1.0;
+
+  // For B3 = diag(0, 0, 1, -1)
+  alg(10, 14) = 1.0;
+  alg(15, 14) = -1.0;
+
+  return alg;
+}
+
+inline Eigen::Matrix<double, 15, 16> setAlgtoVecMatrix() {
+  Eigen::Matrix<double, 15, 16> mat;
+  mat <<
+    0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.,  0.,
+    1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,  0.,
+    1., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0.,  0.,
+    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., -1.;
+  return mat;
+}
+
+// ALG_TO_VEC * VEC_TO_ALG is equals to I_15x15
+const Eigen::Matrix<double, 16, 15> VEC_TO_ALG = setVecToAlgMatrix();
+const Eigen::Matrix<double, 15, 16> ALG_TO_VEC = setAlgtoVecMatrix();
+
+namespace gtsam {
+// NOTE(hlim): Strictly speaking, it should be expressed as SL(4, ℝ), 
+// but for simplicity, we omit ℝ, assuming our target is over the real numbers.
+class GTSAM_UNSTABLE_EXPORT SL4 : public LieGroup<SL4, 15> {
+ public:
+  static const size_t dimension = 15;
+
+ protected:
+  Matrix44 T_;
+
+ public:
+  /// @name Standard Constructors
+  /// @{
+
+  /// Default constructor initializes at origin
+  SL4() : T_(Matrix44::Identity()) {}
+
+  /// Copy constructor
+  SL4(const Matrix44& pose);
+
+  SL4(const SL4& pose);
+
+  /** print with optional string */
+  void print(const std::string& s = "") const;
+
+  /** assert equality up to a tolerance */
+  bool equals(const SL4& SL4, double tol = 1e-9) const;
+
+  inline const Matrix44& matrix() const {
+    return T_;
+  }
+
+  /// @}
+  /// @name Manifold
+  /// @{
+
+  /// Dimensionality of tangent space = 15 DOF - used to autodetect sizes
+  inline static size_t Dim() { return dimension; }
+
+  /// Dimensionality of tangent space = 15 DOF
+  inline size_t dim() const { return dimension; }
+
+  SL4 retract(const Vector& v, SL4Jacobian Horigin,
+                   SL4Jacobian Hv) const;
+
+  Vector localCoordinates(const SL4& p2, SL4Jacobian Horigin,
+                          SL4Jacobian Hp2) const;
+
+  /// @}
+  /// @name Group
+  /// @{
+
+  /// identity for group operation
+  static SL4 Identity() { return SL4(); }
+
+  /// inverse transformation with derivatives
+  SL4 inverse(SL4Jacobian H1 = {}) const;
+
+  /// compose this transformation onto another (first *this and then p2)
+  SL4 compose(const SL4& p2, SL4Jacobian H1 = {},
+                   SL4Jacobian H2 = {}) const;
+
+  /// compose syntactic sugar
+  inline SL4 operator*(const SL4& p) const { return compose(p); }
+
+  /**
+   * Return relative pose between p1 and p2, in p1 coordinate frame
+   * as well as optionally the derivatives
+   */
+  SL4 between(const SL4& p2, SL4Jacobian H1 = {},
+                   SL4Jacobian H2 = {}) const;
+
+  static Matrix44 vectorToAlgebra(const Vector& xi) {
+    assert(xi.size() == 15);
+    Matrix44 mat;
+    const double d11 =  xi(12);
+    const double d22 = -xi(12) + xi(13);
+    const double d33 = -xi(13) + xi(14);
+    const double d44 = -xi(14);
+
+    mat  <<   d11,  xi(0), xi(1), xi(2),
+            xi(3),    d22, xi(4), xi(5),
+            xi(6),  xi(7), d33,   xi(8),
+            xi(9), xi(10), xi(11), d44;
+
+    return mat;
+  } 
+
+  static Vector algebraToVector(const Matrix44& mat) {
+    Vector vec(15);
+    const double x12 = mat(0, 0);
+    const double x13 = mat(1, 1) + x12;
+    const double x14 = -mat(3, 3);
+    vec << mat(0, 1), mat(0, 2), mat(0, 3), mat(1, 0), mat(1, 2), mat(1, 3),
+           mat(2, 0), mat(2, 1), mat(2, 3), mat(3, 0), mat(3, 1), mat(3, 2),
+           x12, x13, x14;
+    return vec;
+  }
+
+  /// Exponential map at identity - create a rotation from canonical coordinates
+  static SL4 Expmap(const Vector& xi);
+
+  /// Log map at identity - return the canonical coordinates of this rotation
+  static Vector Logmap(const Matrix44& T);
+
+  static Vector Logmap(const SL4& p);
+
+
+  /// @}
+  ///
+  Matrix15x15 AdjointMap() {
+    Matrix44 H_inv_T = T_.inverse().transpose();
+    Matrix16x16 C_H;
+
+    // Kronecker product H ⊗ H^{-T}
+    for (int i = 0; i < 4; ++i)
+      for (int j = 0; j < 4; ++j)
+        C_H.block<4, 4>(i * 4, j * 4) = T_(i, j) * H_inv_T;
+
+    return ALG_TO_VEC * C_H * VEC_TO_ALG; 
+  }
+
+ private:
+#ifdef GTSAM_ENABLE_BOOST_SERIALIZATION  
+  // Serialization function
+  friend class boost::serialization::access;
+  template <class Archive>
+  void serialize(Archive& ar, const unsigned int /*version*/) {
+    ar& BOOST_SERIALIZATION_NVP(T_);
+  }
+#endif
+};  // \class Pose4DoF
+
+template <>
+struct traits<SL4> : public internal::LieGroup<SL4> {
+  using ChartJacobian = SL4Jacobian;
+
+  static SL4 Identity() { return SL4(); }
+
+  // NOTE(hlim) In PGO, this function is not used
+  static SL4 Compose(const SL4& g, const SL4& h,
+                            SL4Jacobian H1 = {},
+                            SL4Jacobian H2 = {}) {
+    return g.compose(h, H1, H2);
+  }
+
+  static SL4 Between(const SL4& g, const SL4& h,
+                          SL4Jacobian H1 = {},
+                          SL4Jacobian H2 = {}) {
+    return g.between(h, H1, H2);
+  }
+
+  // NOTE(hlim) In PGO, this function is not used
+  static SL4 Inverse(const SL4& g, SL4Jacobian H = {}) {
+    return g.inverse(H);
+  }
+
+  static SL4 Expmap(const Vector& xi, SL4Jacobian H = {}) {
+    return SL4::Expmap(xi);
+  }
+
+  static Vector Logmap(const SL4& g, SL4Jacobian H = {}) {
+    return SL4::Logmap(g);
+  }
+};
+
+template <>
+struct traits<const SL4> : public traits<SL4> {};
+
+}  // namespace gtsam
