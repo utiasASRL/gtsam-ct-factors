3e42abe9dedcafec1ae229906b265de72e6c48c7
Frank Dellaert <dellaert@gmail.com>
2026-01-31 19:40:38 -0500

    Fixed to use @rpath

--- cmake/HandleGlobalBuildFlags.cmake
+++ cmake/HandleGlobalBuildFlags.cmake
@@ -20,7 +20,7 @@ endif()
 
 if (APPLE AND GTSAM_SHARED_LIB)
     # Set the default install directory on macOS
-    set(CMAKE_INSTALL_NAME_DIR "lib")
+    set(CMAKE_INSTALL_NAME_DIR "@rpath")
 endif()
 
 ###############################################################################
