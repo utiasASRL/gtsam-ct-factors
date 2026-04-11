bc818fcae041ad109e873d054f7289c901b3d3d7
Frank Dellaert <dellaert@gmail.com>
2026-04-11 10:01:22 -0400

    Refactor bearing factors

--- gtsam/sam/BearingFactor.h
+++ gtsam/sam/BearingFactor.h
@@ -18,13 +18,17 @@
 
 #pragma once
 
-#include <gtsam/nonlinear/ExpressionFactor.h>
+#include <gtsam/base/numericalDerivative.h>
+#include <gtsam/nonlinear/NoiseModelFactorN.h>
+
+#include <type_traits>
 
 namespace gtsam {
 
 // forward declaration of Bearing functor, assumed partially specified
 template <typename A1, typename A2>
 struct Bearing;
+class Unit3;
 
 /**
  * Binary factor for a bearing measurement
@@ -34,53 +38,86 @@ struct Bearing;
  */
 template <typename A1, typename A2,
           typename T = typename Bearing<A1, A2>::result_type>
-struct BearingFactor : public ExpressionFactorN<T, A1, A2> {
-  typedef ExpressionFactorN<T, A1, A2> Base;
+struct BearingFactor : public NoiseModelFactorN<A1, A2> {
+  typedef BearingFactor<A1, A2, T> This;
+  typedef NoiseModelFactorN<A1, A2> Base;
+
+  T measured_;
 
   /// default constructor
-  BearingFactor() {}
+  BearingFactor() = default;
 
-  /// primary constructor
+  /// Construct from keys, a bearing measurement, and a noise model.
   BearingFactor(Key key1, Key key2, const T& measured,
                 const SharedNoiseModel& model)
-      : Base({key1, key2}, model, measured) {
-    this->initialize(expression({key1, key2}));
+      : Base(model, key1, key2), measured_(measured) {}
+
+  /// @return a deep copy of this factor
+  gtsam::NonlinearFactor::shared_ptr clone() const override {
+    return std::static_pointer_cast<gtsam::NonlinearFactor>(
+        gtsam::NonlinearFactor::shared_ptr(new This(*this)));
   }
 
-  // Return measurement expression
-  Expression<T> expression(const typename Base::ArrayNKeys &keys) const override {
-    Expression<A1> a1_(keys[0]);
-    Expression<A2> a2_(keys[1]);
-    return Expression<T>(Bearing<A1, A2>(), a1_, a2_);
-  }
+  /// @return the measurement
+  const T& measured() const { return measured_; }
 
   /// print
   void print(const std::string& s = "",
              const KeyFormatter& kf = DefaultKeyFormatter) const override {
     std::cout << s << "BearingFactor" << std::endl;
     Base::print(s, kf);
-  }
-  
-  Vector evaluateError(const A1& a1, const A2& a2,
-    OptionalMatrixType H1 = OptionalNone, OptionalMatrixType H2 = OptionalNone) const {
-    std::vector<Matrix> Hs(2);
-    const auto &keys = Factor::keys();
-    const Vector error = unwhitenedError(
-      {{keys[0], genericValue(a1)}, {keys[1], genericValue(a2)}}, 
-      Hs);
-    if (H1) *H1 = Hs[0];
-    if (H2) *H2 = Hs[1];
-    return error;
+    traits<T>::Print(measured_, "  measured: ");
   }
 
+  /// Check equality up to a tolerance.
+  bool equals(const NonlinearFactor& f, double tol = 1e-9) const override {
+    const This* p = dynamic_cast<const This*>(&f);
+    return p != nullptr && Base::equals(f, tol) &&
+           traits<T>::Equals(measured_, p->measured_, tol);
+  }
+
+  /// Evaluate the unwhitened bearing error and optional Jacobians.
+  Vector evaluateError(const A1& a1, const A2& a2,
+                       OptionalMatrixType H1 = OptionalNone,
+                       OptionalMatrixType H2 = OptionalNone) const override {
+    Matrix Hpred1, Hpred2;
+    const T predicted = Bearing<A1, A2>()(a1, a2, H1 ? &Hpred1 : nullptr,
+                                          H2 ? &Hpred2 : nullptr);
+    if constexpr (std::is_same_v<T, Unit3>) {
+      const auto localError = [&](const T& value) -> Vector {
+        return -traits<T>::Local(value, measured_);
+      };
+      const Vector error = localError(predicted);
+      if (H1) {
+        const Matrix HerrorPred =
+            numericalDerivative11<Vector, T>(localError, predicted);
+        *H1 = HerrorPred * Hpred1;
+      }
+      if (H2) {
+        const Matrix HerrorPred =
+            numericalDerivative11<Vector, T>(localError, predicted);
+        *H2 = HerrorPred * Hpred2;
+      }
+      return error;
+    } else {
+      Matrix Hlocal;
+      const Vector error = -traits<T>::Local(
+          predicted, measured_, (H1 || H2) ? &Hlocal : nullptr, OptionalNone);
+      if (H1) *H1 = -Hlocal * Hpred1;
+      if (H2) *H2 = -Hlocal * Hpred2;
+      return error;
+    }
+  }
 
  private:
 #if GTSAM_ENABLE_BOOST_SERIALIZATION
   friend class boost::serialization::access;
+  /// Serialize this factor.
   template <class ARCHIVE>
   void serialize(ARCHIVE& ar, const unsigned int /*version*/) {
     ar& boost::serialization::make_nvp(
-        "Base", boost::serialization::base_object<Base>(*this));
+        "NoiseModelFactor2", boost::serialization::base_object<Base>(*this));
+    ar& BOOST_SERIALIZATION_NVP(measured_);
   }
 #endif
 };  // BearingFactor
